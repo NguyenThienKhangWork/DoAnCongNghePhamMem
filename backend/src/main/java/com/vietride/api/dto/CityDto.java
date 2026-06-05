a1ea87b315adeece42642c66d54efd5071b2a108
khang nguyen thien <khangnt2157@ut.edu.vn>
2026-06-06 06:05:09 +0700

    Delete backend directory

--- backend/src/main/java/com/vietride/api/dto/CityDto.java
+++
@@ -1,4 +0,0 @@
-package com.vietride.api.dto;
-
-public record CityDto(Long id, String name, String region) {
-}
--- backend/src/main/java/com/vietride/api/repository/BusOperatorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vietride.api.repository;
-
-import com.vietride.api.model.BusOperator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusOperatorRepository extends JpaRepository<BusOperator, Long> {
-}
